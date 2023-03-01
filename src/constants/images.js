4ee85599b9778151f4c39c9241d9d3a153a9436b
benNjogu <kamandemachafi@gmail.com>
2023-03-01 18:51:57 +0300

    Our Mock Data_Store

--- src/constants/images.js
+++ src/constants/images.js
@@ -4,10 +4,90 @@ import sneakers from "../assets/images/sneakers.jpg";
 import womens from "../assets/images/womens.webp";
 import mens from "../assets/images/mens.jpg";
 
+import brown_brim from "../assets/images/brown_brim.png";
+import bluebeanie from "../assets/images/beanie.png";
+import brown_cowboy from "../assets/images/cowboy.png";
+import grey_brim from "../assets/images/brim.webp";
+import green_beanie from "../assets/images/green_beanie.png";
+import palm_tree_cap from "../assets/images/palm_tree_cap.png";
+import red_beanie from "../assets/images/red_beanie.png";
+import wolf_cap from "../assets/images/wolf_cap.png";
+import blue_snapback from "../assets/images/blue_snapback.png";
+
+import adidas_nmd from "../assets/images/adidas_nmd.png";
+import adidas_yeezy from "../assets/images/adidas_yeezy.png";
+import black_converse from "../assets/images/black_converse.png";
+import nike_white from "../assets/images/nike_airforce.png";
+import nike_red from "../assets/images/nike_red.png";
+import nike_brown from "../assets/images/brown_high.png";
+import jordan_limited from "../assets/images/air_jordan.png";
+import timberlands from "../assets/images/timberlands.png";
+
+import black_shearing from "../assets/images/black_shearing.webp";
+import blue_jean from "../assets/images/blue_jean.png";
+import grey_jean from "../assets/images/grey_jean.webp";
+import brown_shearing from "../assets/images/brown_shearing.png";
+import tan_trench from "../assets/images/tan_trench.webp";
+
+import tank_top from "../assets/images/tank_top.webp";
+import floral_blouse from "../assets/images/floral_blouse.png";
+import floral_dress from "../assets/images/floral_dress.webp";
+import red_dots from "../assets/images/red_dots.png";
+import striped_sweater from "../assets/images/striped_sweater.png";
+import yellow_track from "../assets/images/yellow_track.png";
+import white_blouse from "../assets/images/white_blouse.png";
+
+import camo_vest from "../assets/images/camo_down.webp";
+import floral_tshirt from "../assets/images/floral_tshirt.png";
+import bandw_longsleeve from "../assets/images/bandw_longsleeve.png";
+import pink_t_shirt from "../assets/images/pink-t-shirt.png";
+import jean_longsleeve from "../assets/images/jean_longsleeve.webp";
+import burgundy from "../assets/images/burgundy.png";
+
 export default {
   hat,
   jackets,
   sneakers,
   womens,
   mens,
+
+  brown_brim,
+  bluebeanie,
+  brown_cowboy,
+  grey_brim,
+  green_beanie,
+  palm_tree_cap,
+  red_beanie,
+  wolf_cap,
+  blue_snapback,
+
+  adidas_nmd,
+  adidas_yeezy,
+  black_converse,
+  nike_white,
+  nike_red,
+  nike_brown,
+  jordan_limited,
+  timberlands,
+
+  black_shearing,
+  blue_jean,
+  grey_jean,
+  brown_shearing,
+  tan_trench,
+
+  tank_top,
+  floral_blouse,
+  floral_dress,
+  red_dots,
+  striped_sweater,
+  yellow_track,
+  white_blouse,
+
+  camo_vest,
+  floral_tshirt,
+  bandw_longsleeve,
+  pink_t_shirt,
+  jean_longsleeve,
+  burgundy,
 };
---
+++ src/data_store/data.js
@@ -0,0 +1,249 @@
+import { images } from "../constants";
+
+const SHOP_DATA = [
+  {
+    id: 1,
+    title: "Hats",
+    routeName: "hats",
+    items: [
+      {
+        id: 1,
+        name: "Brown Brim",
+        imageUrl: images.brown_brim,
+        price: 25,
+      },
+      {
+        id: 2,
+        name: "Blue Beanie",
+        imageUrl: images.bluebeanie,
+        price: 18,
+      },
+      {
+        id: 3,
+        name: "Brown Cowboy",
+        imageUrl: images.brown_cowboy,
+        price: 35,
+      },
+      {
+        id: 4,
+        name: "Grey Brim",
+        imageUrl: images.grey_brim,
+        price: 25,
+      },
+      {
+        id: 5,
+        name: "Green Beanie",
+        imageUrl: images.green_beanie,
+        price: 18,
+      },
+      {
+        id: 6,
+        name: "Palm Tree Cap",
+        imageUrl: images.palm_tree_cap,
+        price: 14,
+      },
+      {
+        id: 7,
+        name: "Red Beanie",
+        imageUrl: images.red_beanie,
+        price: 18,
+      },
+      {
+        id: 8,
+        name: "Wolf Cap",
+        imageUrl: images.wolf_cap,
+        price: 14,
+      },
+      {
+        id: 9,
+        name: "Blue Snapback",
+        imageUrl: images.blue_snapback,
+        price: 16,
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Sneakers",
+    routeName: "sneakers",
+    items: [
+      {
+        id: 1,
+        name: "Adidas NMD",
+        imageUrl: images.adidas_nmd,
+        price: 220,
+      },
+      {
+        id: 2,
+        name: "Adidas Yeezy",
+        imageUrl: images.adidas_yeezy,
+        price: 280,
+      },
+      {
+        id: 3,
+        name: "Black Converse",
+        imageUrl: images.black_converse,
+        price: 110,
+      },
+      {
+        id: 4,
+        name: "Nike White AirForce",
+        imageUrl: images.nike_white,
+        price: 160,
+      },
+      {
+        id: 5,
+        name: "Nike Red High Tops",
+        imageUrl: images.nike_red,
+        price: 160,
+      },
+      {
+        id: 6,
+        name: "Nike Brown High Tops",
+        imageUrl: images.nike_brown,
+        price: 160,
+      },
+      {
+        id: 7,
+        name: "Air Jordan Limited",
+        imageUrl: images.jordan_limited,
+        price: 190,
+      },
+      {
+        id: 8,
+        name: "Timberlands",
+        imageUrl: images.timberlands,
+        price: 200,
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Jackets",
+    routeName: "jackets",
+    items: [
+      {
+        id: 1,
+        name: "Black Jean Shearling",
+        imageUrl: images.black_shearing,
+        price: 125,
+      },
+      {
+        id: 2,
+        name: "Blue Jean Jacket",
+        imageUrl: images.blue_jean,
+        price: 90,
+      },
+      {
+        id: 3,
+        name: "Grey Jean Jacket",
+        imageUrl: images.grey_jean,
+        price: 90,
+      },
+      {
+        id: 4,
+        name: "Brown Shearling",
+        imageUrl: images.brown_shearing,
+        price: 165,
+      },
+      {
+        id: 5,
+        name: "Tan Trench",
+        imageUrl: images.tan_trench,
+        price: 185,
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Womens",
+    routeName: "womens",
+    items: [
+      {
+        id: 1,
+        name: "Blue Tanktop",
+        imageUrl: images.tank_top,
+        price: 25,
+      },
+      {
+        id: 2,
+        name: "Floral Blouse",
+        imageUrl: images.floral_blouse,
+        price: 20,
+      },
+      {
+        id: 3,
+        name: "Floral Dress",
+        imageUrl: images.floral_dress,
+        price: 80,
+      },
+      {
+        id: 4,
+        name: "Red Dots Dress",
+        imageUrl: images.red_dots,
+        price: 80,
+      },
+      {
+        id: 5,
+        name: "Striped Sweater",
+        imageUrl: images.striped_sweater,
+        price: 45,
+      },
+      {
+        id: 6,
+        name: "Yellow Track Suit",
+        imageUrl: images.yellow_track,
+        price: 135,
+      },
+      {
+        id: 7,
+        name: "White Blouse",
+        imageUrl: images.white_blouse,
+        price: 20,
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Mens",
+    routeName: "mens",
+    items: [
+      {
+        id: 1,
+        name: "Camo Down Vest",
+        imageUrl: images.camo_vest,
+        price: 325,
+      },
+      {
+        id: 2,
+        name: "Floral T-shirt",
+        imageUrl: images.floral_tshirt,
+        price: 20,
+      },
+      {
+        id: 3,
+        name: "Black & White Longsleeve",
+        imageUrl: images.bandw_longsleeve,
+        price: 25,
+      },
+      {
+        id: 4,
+        name: "Pink T-shirt",
+        imageUrl: images.pink_t_shirt,
+        price: 25,
+      },
+      {
+        id: 5,
+        name: "Jean Long Sleeve",
+        imageUrl: images.jean_longsleeve,
+        price: 40,
+      },
+      {
+        id: 6,
+        name: "Burgundy T-shirt",
+        imageUrl: images.burgundy,
+        price: 25,
+      },
+    ],
+  },
+];
