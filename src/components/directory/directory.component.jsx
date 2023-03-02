8cd5c154639d320c41d6942f0f79ed617f544931
benNjogu <kamandemachafi@gmail.com>
2023-03-02 09:41:25 +0300

    Cleaned

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -3,49 +3,13 @@ import React, { Component } from "react";
 import MenuItem from "./../menu-item/menu-item.component";
 import "./directory.styles.scss";
 
-import { images } from "../../constants";
-
+import { SECTIONS } from "../../data_store/data";
 export default class Directory extends Component {
   constructor() {
     super();
 
     this.state = {
-      sections: [
-        {
-          title: "hats",
-          imageUrl: images.hat,
-          id: 1,
-          linkUrl: "shop/hats",
-        },
-        {
-          title: "jackets",
-          imageUrl: images.jackets,
-          id: 2,
-          linkUrl: "shop/jackets",
-        },
-        {
-          title: "sneakers",
-          imageUrl: images.sneakers,
-          id: 3,
-          linkUrl: "shop/sneakers",
-        },
-        {
-          title: "womens",
-          imageUrl: images.womens,
-          size: "large",
-          id: 4,
-          size: "large",
-          linkUrl: "shop/womens",
-        },
-        {
-          title: "mens",
-          imageUrl: images.mens,
-          size: "large",
-          id: 5,
-          size: "large",
-          linkUrl: "shop/mens",
-        },
-      ],
+      sections: SECTIONS,
     };
   }
 
