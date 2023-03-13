64f7afb898a53e5f350a563899f9dddf922cb081
benNjogu <kamandemachafi@gmail.com>
2023-03-13 09:18:57 +0300

    Styled components part 1 using css

--- src/components/header/header.componet.jsx
+++ src/components/header/header.componet.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { auth } from "../../firebase/firebase.utils";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
@@ -10,34 +9,33 @@ import { selectCartHidden } from "../../redux/cart/cart.selectors";
 import { ReactComponent as Logo } from "../../assets/logo/crown.svg";
 import CartIcon from "../cart-icon/cart-icon.component";
 import CartDropdown from "./../cart-dropdown/cart-dropdown.component";
-import "./header.styles.scss";
+
+import {
+  HeaderContainer,
+  LogoContainer,
+  OptionsContainer,
+  OptionLink,
+  OptionDiv,
+} from "./header.style";
 
 const Header = ({ currentUser, hidden }) => {
   return (
-    <div className="header">
-      <Link className="logo-container" to={"/"}>
-        <Logo className="logo" />
-      </Link>
-      <div className="options">
-        <Link className="option" to={"/shop"}>
-          SHOP
-        </Link>
-        <Link className="option" to={"/contact"}>
-          CONTACT
-        </Link>
+    <HeaderContainer>
+      <LogoContainer to={"/"}>
+        <Logo />
+      </LogoContainer>
+      <OptionsContainer>
+        <OptionLink to={"/shop"}>SHOP</OptionLink>
+        <OptionLink to={"/contact"}>CONTACT</OptionLink>
         {currentUser ? (
-          <div className="option" onClick={() => auth.signOut()}>
-            SIGN OUT
-          </div>
+          <OptionDiv onClick={() => auth.signOut()}>SIGN OUT</OptionDiv>
         ) : (
-          <Link className="option" to="/signin">
-            SIGN IN
-          </Link>
+          <OptionLink to="/signin">SIGN IN</OptionLink>
         )}
         <CartIcon />
-      </div>
+      </OptionsContainer>
       {!hidden && <CartDropdown />}
-    </div>
+    </HeaderContainer>
   );
 };
 
