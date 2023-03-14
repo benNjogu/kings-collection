53b8c845936b40d7a5adfa51f83df0866a8078f7
benNjogu <kamandemachafi@gmail.com>
2023-03-14 15:34:49 +0300

    Using observable pattern

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,13 +1,50 @@
-import React from "react";
+import React, { Component } from "react";
 
+import {
+  firestore,
+  convertCollectionsSnapshotToMap,
+} from "../../firebase/firebase.utils";
+
+import { connect } from "react-redux";
+
+import WithSpinner from "../../components/with-spinner/with-spinner.component";
 import CollectionOverview from "../../components/collection-overview/collection-overview.component";
+import { updateCollections } from "./../../redux/shop/shop.actions";
 
-const ShopPage = () => {
-  return (
-    <div className="shop-page">
-      <CollectionOverview />
-    </div>
-  );
-};
+const CollectionOverViewWithSpinner = WithSpinner(CollectionOverview);
+class ShopPage extends Component {
+  state = {
+    loading: true,
+  };
 
-export default ShopPage;
+  unsubscribeFromSnapshot = null;
+
+  componentDidMount() {
+    const { updateCollections } = this.props;
+
+    const collectionRef = firestore.collection("collections");
+
+    collectionRef.onSnapshot(async (snapshot) => {
+      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+      updateCollections(collectionsMap);
+      this.setState({ loading: false });
+    });
+  }
+
+  render() {
+    const { loading } = this.state;
+
+    return (
+      <div className="shop-page">
+        <CollectionOverViewWithSpinner isLoading={loading} {...this.props} />
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  updateCollections: (collectionsMap) =>
+    dispatch(updateCollections(collectionsMap)),
+});
+
+export default connect(null, mapDispatchToProps)(ShopPage);
