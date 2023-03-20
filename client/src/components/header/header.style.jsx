fcada5c3896b69f91fc83980fc8dbe7de0aa8de4
benNjogu <kamandemachafi@gmail.com>
2023-03-20 11:50:52 +0300

    Sticking the Navbar at the top and positioning the icon at the center.

--- client/src/components/header/header.style.jsx
+++ client/src/components/header/header.style.jsx
@@ -2,17 +2,23 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 export const HeaderContainer = styled.div`
+  position: sticky;
+  top: 0;
   height: 70px;
   width: 100%;
   display: flex;
   justify-content: space-between;
   margin-bottom: 25px;
+  z-index: 1;
 `;
 
 export const LogoContainer = styled(Link)`
   height: 100%;
   width: 70px;
-  padding: 25px;
+  padding: 16px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const OptionsContainer = styled.div`
